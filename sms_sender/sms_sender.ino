f813140bcfc18d8e51c8e97e54d4191fdef26891
Mantas Zinkevicius <mantas.zinkevicius@hotmail.com>
2018-03-20 11:24:36 +0100

    time manipulation requires some string manipulation work in progress

--- sms_sender/sms_sender.ino
+++ sms_sender/sms_sender.ino
@@ -1,22 +1,78 @@
+#include <Time.h>
+#include <TimeLib.h>
 #include <SoftwareSerial.h>
 #include <gprs.h>
  
-//SIM800 TX is connected to Arduino D8
-//#define SIM800_TX_PIN 8
- 
-//SIM800 RX is connected to Arduino D7
-//#define SIM800_RX_PIN 7
- 
-//Create software serial object to communicate with SIM800
-//SoftwareSerial serialSIM800(SIM800_TX_PIN,SIM800_RX_PIN);
-
 GPRS gprs;
+
+char* number = (char*)"+4591520714";
+char* message = (char*)"System has been armed\r\n";
+char tempHours[5];
+char tempMinutes[5];
+char tempSeconds[5];
+char* timeString = (char*)"";
+char* smsMessage = (char*)"";
+time_t current_time;
  
 void setup() {
   //Begin serial comunication with Arduino and Arduino IDE (Serial Monitor)
   Serial.begin(9600);
   while(!Serial);
+  
+  //---------TIME default Setup----------------------//
+    
+  //setTime(hours, minutes, seconds, days, months, years);
+  setTime(15, 59, 3, 16, 3, 18);
+  time_t current_time = now();
+  if (timeStatus() == timeNotSet)
+  {
+    error:
+    Serial.println("ERROR time set incorrect");
+  }
+  else
+  {
+    Serial.println("OK Time set correct");
+  }
+  
+  converTime();
+  
+  
+  //Serial.println("Sending SMS ...");
+  //sendSMS(smsMessage);
+}
 
+void converTime(){
+  //FORMAT TIME
+    
+    //HOURS
+    
+    sprintf(tempHours, "%d", hour(current_time));
+    strcat(timeString, tempHours);
+    strcat(timeString, ":");
+
+    //MINUTES
+    sprintf(tempMinutes, "%d",minute(current_time));
+    strcat(timeString, tempMinutes);
+    strcat(timeString, ":");
+
+    //SECONDS
+    sprintf(tempSeconds, "%d", second(current_time));
+    strcat(timeString, tempSeconds);
+    
+    //strcat(message, timeString);
+    //String temp = message + String(timeString);
+    char* s = (char*)"abc";
+    for(int i = 0; i <= strlen(s); i++){
+      Serial.println(s[i]);
+      
+    }
+}
+
+void loop() {
+  
+}
+
+void sendSMS(char *msg){
   Serial.println("GPRS - Send SMS Test ...");
   gprs.preInit();
   delay(1000);
@@ -25,41 +81,6 @@ void setup() {
       Serial.print("init error\r\n");
   } 
   Serial.println("Init success, start to send SMS message...");
-  gprs.sendSMS("+4591520714","hello,world"); //define phone number and text
+  gprs.sendSMS(number, msg); //define phone number and text
+}
 
-   
-  //Being serial communication witj Arduino and SIM800
-//  serialSIM800.begin(9600);
-//  delay(1000);
-//   
-//  Serial.println("Setup Complete!");
-//
-//  //Set SMS format to ASCII
-//  serialSIM800.write("AT+CMGF=1\r\n");
-//  delay(1000);
-// 
-//  //Send new SMS command and message number
-//  serialSIM800.write("AT+CMGS=\"+4591520714\"\r\n");
-//  delay(1000);
-//   
-//  //Send SMS content
-//  serialSIM800.write("TEST\r\nHello");
-//  delay(1000);
-//   
-//  //Send Ctrl+Z / ESC to denote SMS message is complete
-//  serialSIM800.write((char)26);
-//  delay(1000);
-//     
-//  Serial.println("SMS Sent!");
-}
- 
-void loop() {
-//  //Read SIM800 output (if available) and print it in Arduino IDE Serial Monitor
-//  if(serialSIM800.available()){
-//    Serial.write(serialSIM800.read());
-//  }
-//  //Read Arduino IDE Serial Monitor inputs (if available) and send them to SIM800
-//  if(Serial.available()){    
-//    serialSIM800.write(Serial.read());
-//  }
-}
